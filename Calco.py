a5e830596f69cc2d9de327501f6e9e00834d7c7d
Ridhima Tigadi <44704352+divine-dumpling@users.noreply.github.com>
2021-10-02 15:17:50 +0530

    Update Calco.py

--- Calco.py
+++ Calco.py
@@ -22,9 +22,6 @@ loaded_model_l = pickle.load(open('linear_model.sav', 'rb'))
 landing = """<center><h2>Select your Choice</h2></center><center><h3>Warmup ⚪ Stats ⚪ Predict</h3></center>""" 
 
 stats = """<center><h3><b><u>Statistics during Training the model</u></b></h3></center>"""
-  background: #ff0099; 
-  background: -webkit-linear-gradient(to right, #ff0099, #493240); 
-  background: linear-gradient(to right, #ff0099, #493240);"
 
 st.markdown("""
 <style>
